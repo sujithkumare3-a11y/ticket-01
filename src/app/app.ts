f342ca076a4eaf1d44038c783a005472c1b941ec
PRATHISH-PK <prathishpk81@gmail.com>
2026-01-21 09:27:16 +0530

    commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+// import { onclick } from '@angular/core/directives/onclick';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+openlinkedIn: any;
+openlinkedin() {
+throw new Error('Method not implemented.');
+}
+toggleDropdown() {
+throw new Error('Method not implemented.');
+}
+closeDropdown() {
+throw new Error('Method not implemented.');
+}
 bookAppointment() {
 throw new Error('Method not implemented.');
 }
@@ -16,3 +27,26 @@ doctor: any;
 }
 
  
+ 
+   
+
+
+
+  
+
+ 
+
+  
+
+function toggleDropdown() {
+  throw new Error('Function not implemented.');
+}
+
+function closeDropdown() {
+  throw new Error('Function not implemented.');
+}
+
+function openlinkdin(): void {
+  throw new Error('Function not implemented.');
+}
+
